a4d02aae05a297a3131b8d12f1b3aa8bbdff180a
ganesh <gowdaganesh0511@gmail.com>
2024-10-07 21:54:52 +0530

    kth positive missing number

---
+++ Binary-Search/leetcode1539M.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int findKthPositive(vector<int>& arr, int k) {
+        int low=0,high=arr.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int missing=arr[mid]-mid-1;
+            if(missing<k){
+                low=mid+1;
+
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low+k;
+
+    }
+int main(){
+  vector<int> arr={2,5,6,7};
+  cout<<findKthPositive(arr,6);
+  return 0;
+
+}
